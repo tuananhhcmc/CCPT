2b0ab4b4f94b23ef1c0990d5a329b128e52490e7
arTdontlie <tuananh281003@gmail.com>
2025-04-09 22:37:29 +0700

    update user

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+require_once __DIR__ . '/../Models/NhanVien.php';
+
+class NhanVienController
+{
+    private $nhanvienModel;
+    private $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+        $this->nhanvienModel = new NhanVien($this->conn);
+    }
+
+    public function index()
+    {
+        $limit = 5;
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+        $start = ($page - 1) * $limit;
+
+        $nhanviens = $this->nhanvienModel->getAll($start, $limit);
+        $total_records = $this->nhanvienModel->getTotalRecords();
+        $total_pages = ceil($total_records / $limit);
+
+        include 'app/Views/nhanvien/index.php';
+    }
+
+    public function create()
+    {
+        if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
+            include 'app/Views/nhanvien/create.php';
+        } else {
+            echo "Bạn không có quyền thực hiện thao tác này.";
+        }
+    }
+
+
+    public function store()
+    {
+        if ($_SESSION['role'] == 'admin') {
+            $Ma_NV = $_POST['Ma_NV'];
+            $Ten_NV = $_POST['Ten_NV'];
+            $Phai = $_POST['Phai'];
+            $Noi_Sinh = $_POST['Noi_Sinh'];
+            $Ma_Phong = $_POST['Ma_Phong'];
+            $Luong = $_POST['Luong'];
+
+            if ($this->nhanvienModel->create($Ma_NV, $Ten_NV, $Phai, $Noi_Sinh, $Ma_Phong, $Luong)) {
+                header("Location: index.php");
+            } else {
+                echo "Có lỗi xảy ra khi thêm nhân viên. Xem chi tiết bên trên.";
+            }
+        } else {
+            echo "Bạn không có quyền thực hiện thao tác này.";
+        }
+    }
+
+
+}
+?>
